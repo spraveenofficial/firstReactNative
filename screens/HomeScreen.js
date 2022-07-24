f0a2a3f2170424216e48d30cbb4578a18fa1a159
Praveen <spraveen593@gmail.com>
2022-07-24 15:28:48 +0530

    feat: :sparkles: Added Tailwind CSS / React Native Navigation/ Navigation Stack

---
+++ screens/HomeScreen.js
@@ -0,0 +1,32 @@
+import { View, Text, SafeAreaView, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+const HomeScreen = () => {
+    const navigation = useNavigation()
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        })
+    }, []);
+    return (
+        <SafeAreaView>
+            <Text className="text-red-500">
+                <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+                    <Image
+                        source={{
+                            uri: 'https://links.papareact.com/wru',
+                        }}
+                        className='h-7 w-7 bg-gray-300 rounded-full'
+                    />
+                    <View>
+                        <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+                        <Text className="font-bold font-xl">Current Location</Text>
+                    </View>
+                </View>
+
+            </Text>
+        </SafeAreaView>
+    )
+}
+
+export default HomeScreen
