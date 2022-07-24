9289e3fdf6f7e065b328ea14baf28dfa8b9ddfdf
Praveen <spraveen593@gmail.com>
2022-07-24 16:15:04 +0530

    feat: :zap: Added Font

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import { View, Text, SafeAreaView, Image } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
+import { ChevronDownIcon, SearchIcon, UserIcon } from "react-native-heroicons/outline"
 const HomeScreen = () => {
     const navigation = useNavigation()
     useLayoutEffect(() => {
@@ -9,22 +10,25 @@ const HomeScreen = () => {
         })
     }, []);
     return (
-        <SafeAreaView>
-            <Text className="text-red-500">
-                <View className='flex-row pb-3 items-center mx-4 space-x-2'>
-                    <Image
-                        source={{
-                            uri: 'https://links.papareact.com/wru',
-                        }}
-                        className='h-7 w-7 bg-gray-300 rounded-full'
-                    />
-                    <View>
-                        <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-                        <Text className="font-bold font-xl">Current Location</Text>
-                    </View>
+        <SafeAreaView className="bg-white py-4">
+            <View className='flex-row items-center mx-4 space-x-2'>
+                <Image
+                    source={{
+                        uri: 'https://links.papareact.com/wru',
+                    }}
+                    className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+                />
+                <View className="flex-1">
+                    <Text className="font-bold text-gray-400 text-xs">
+                        Deliver Now!
+                    </Text>
+                    <Text className="font-bold font-xl justify-center items-center">
+                        Current Location
+                        <ChevronDownIcon size={20} color="#00CCBB" />
+                    </Text>
                 </View>
-
-            </Text>
+                <UserIcon size={35} color="#00CCBB" />
+            </View>
         </SafeAreaView>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,24 @@ module.exports = {
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
-
+    // Add custom fonts
+    fonts: ["./assets/fonts"],
+    // Add default fonts
+    fontSizes: {
+        base: "16px",
+        xs: "12px",
+        sm: "14px",
+        md: "16px",
+        lg: "18px",
+        xl: "20px",
+        "2xl": "24px",
+        "3xl": "28px",
+        "4xl": "32px",
+        "5xl": "40px",
+    },
+    fontFamilys: {
+        sans: ["Inter var", "sans-serif"],
+        serif: ["Georgia", "serif"],
+        main: ['all-demi']
+    }
 };
